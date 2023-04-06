d702f553d2ac057f726efadfaefea1d48ee916ee
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-04-06 10:03:15 +0200

    updated challenge-1

--- challenge-1.js
+++ challenge-1.js
@@ -3,8 +3,11 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-let primaryValid = isNaN(primaryPhone);
-let secondaryValid = isNaN(secondaryPhone);
+let primaryValid = primaryPhone;
+let secondaryValid = secondaryPhone;
+
+isNaN(primaryValid) ? primaryValid = false : primaryValid = true;
+isNaN(secondaryValid) ? secondaryValid = false : secondaryValid = true;
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
